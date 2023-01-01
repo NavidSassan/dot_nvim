737b633c1424d6e01ae55226e1bda07fa763c821
Navid Sassan <navid@sassan.ch>
2023-01-01 20:39:46 +0100

    improve sumneko config (lua lsp)

--- init.lua
+++ init.lua
@@ -301,8 +301,23 @@ local servers = {
     jsonls = {},
     sumneko_lua = {
         Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
         },
     },
 }
