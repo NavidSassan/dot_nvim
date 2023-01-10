c763357fd8c8308acc28a090bf7b53fd37ebabb6
Navid Sassan <navid@sassan.ch>
2023-01-10 09:43:23 +0100

    cmp: remove enter mapping

--- init.lua
+++ init.lua
@@ -360,10 +360,6 @@ cmp.setup {
     mapping = cmp.mapping.preset.insert {
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
