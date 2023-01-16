90fff5e8a6876796b05dc2652455b3d956df41c3
Navid Sassan <navid@sassan.ch>
2023-01-16 18:38:35 +0100

    oil.nvim: do not close on <c-c>

--- init.lua
+++ init.lua
@@ -596,5 +596,21 @@ require 'hex'.setup({
 require("dapui").setup()
 
 -- oil.nvim
-require("oil").setup()
+require("oil").setup({
+    keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = "actions.select_vsplit",
+        ["<C-h>"] = "actions.select_split",
+        ["<C-p>"] = "actions.preview",
+        -- ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["g."] = "actions.toggle_hidden",
+    },
+    use_default_keymaps = false,
+})
 vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
