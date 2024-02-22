3fed6e827cfb82fd55f99f9154e474acbf4c175a
Navid Sassan <navid@sassan.ch>
2024-02-22 22:35:42 +0100

    make sure colorscheme are loaded first

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,18 @@
+-- NOTE: since start plugins can possibly change existing highlight groups, it's important to make sure that your main colorscheme is loaded first. To ensure this you can use the priority=1000 field.
+
 return {
 
     {
         "folke/tokyonight.nvim",
         lazy = true,
+        priority = 1000,
         opts = { style = "moon" },
     },
 
     {
         'EdenEast/nightfox.nvim',
         lazy = true,
+        priority = 1000,
     },
 
     {
@@ -20,6 +24,7 @@ return {
     {
         'shaunsingh/solarized.nvim',
         lazy = true,
+        priority = 1000,
     },
 
 }
