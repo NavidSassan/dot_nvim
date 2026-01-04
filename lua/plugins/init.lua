9634ba57d23e62be46b5c1eeeac65b35e3810d77
Navid Sassan <navid@sassan.ch>
2026-01-04 03:43:00 +0100

    add nvim-spider for subword motions

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -515,6 +515,14 @@ return {
     { 'itchyny/vim-qfedit', ft = "qf" },
     { 'chentoast/marks.nvim', event = "VeryLazy", opts = {} },
     {
+        'chrisgrieser/nvim-spider',
+        keys = {
+            { 'w', "<cmd>lua require('spider').motion('w')<CR>", mode = { 'n', 'o', 'x' }, desc = 'Spider w' },
+            { 'e', "<cmd>lua require('spider').motion('e')<CR>", mode = { 'n', 'o', 'x' }, desc = 'Spider e' },
+            { 'b', "<cmd>lua require('spider').motion('b')<CR>", mode = { 'n', 'o', 'x' }, desc = 'Spider b' },
+        },
+    },
+    {
         'nvim-mini/mini.clue',
         event = 'VeryLazy',
         opts = function()
